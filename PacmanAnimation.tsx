ce9884541bdd4f823cfdacafa9d750ee0555235d
audreywqy <q344wang@uwaterloo.ca>
2025-06-20 22:11:18 -0700

    pacman

---
+++ PacmanAnimation.tsx
@@ -0,0 +1,10 @@
+import { generatePacManGame } from 'pacman-contribution-graph';
+
+generatePacManGame({
+	username: 'audreywqy',
+	platform: 'github', // or 'gitlab'
+	theme: 'github-dark', // or 'gitlab-dark'
+	speed: 2,
+	sound: true,
+	output: 'svg' // or 'svg'
+});
